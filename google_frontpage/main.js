0f83e9f5d2c535f287779b6aa64f844bc6ca6ba0
MaximeBrandely <maxime.brandely@gmail.com>
2018-01-11 22:21:12 +0100

    V1

---
+++ google_frontpage/main.js
@@ -0,0 +1,32 @@
+/* Cacher */
+
+ function central(elementscentre) 
+{ 
+  var div = document.getElementById(elementscentre); 
+  div.style.display = "block";
+
+} 
+
+
+
+/* popup */
+
+function popupPage() {
+alert ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
+
+
+/* Récupération valeur */
+
+function searchbox (barrerecherche)
+{
+var searchbox = document.getElementById("searchbox").value;
+alert(searchbox);
+}
+
+/* lien */
+
+function 
+https://www.google.fr/#q=
+
+
